0008935af4e388c27e71fac08430a9e1e5a07bb9
Haris Melunovic <ha121mel@htwg-konstanz.de>
2026-04-15 17:59:10 +0200

    First changes Aufgabenblatt 2

---
+++ script.js
@@ -0,0 +1,11 @@
+// Mobile Menu Toggle 
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBtn = document.querySelector('.mobile-menu-btn');
+    
+    if (menuBtn) {
+        menuBtn.addEventListener('click', function() {
+            // Mobile menu functionality can be added here
+            console.log('Mobile menu clicked');
+        });
+    }
+});
